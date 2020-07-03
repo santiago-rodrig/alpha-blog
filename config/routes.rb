bc646eda2698ce0014298c543d6fd8833ecef37d
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-03 18:44:27 -0400

    feature: articles controller show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'about', to: 'static_pages#about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles, only: :show
 end

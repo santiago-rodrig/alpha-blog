27c1cb498f9ff5d2c891d5b6458e4e2f7fe64254
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-05 12:43:50 -0400

    feature: notice message on article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,18 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to article_path(@article)
+
+    if @article.save
+      flash[:notice] = 'Article was successfully created'
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
   end
 
   private
@@ -22,3 +28,4 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title, :description)
   end
 end
+

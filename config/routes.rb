c86767461d5779bfa16cf2311140d3a102b17681
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-05 12:58:37 -0400

    feature: editing of articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'about', to: 'static_pages#about'
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end

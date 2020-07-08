309f0b89f9e5b93ddd56dfb344032bfb23f0224f
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-08 18:43:09 -0400

    feature: add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ApplicationRecord
+  validates(
+    :name,
+    uniqueness: { case_sensitive: false },
+    presence: true,
+    length: { minimum: 3, maximum: 25 },
+  )
+
+  validates(
+    :email,
+    uniqueness: true,
+    presence: true,
+    length: { maximum: 105 },
+
+    format: {
+      with: URI::MailTo::EMAIL_REGEXP,
+      message: 'format is invalid'
+    }
+  )
 end

f0a138fed799370678c2206ec94594af06e4159b
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-12 19:22:31 -0400

    feature: update users from browser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:update, :edit]
   def new
     @user = User.new
   end
@@ -13,7 +14,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = 'Account details successfully updated.'
+      redirect_to articles_path
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)

6e7d4ebe79716114de5e5ff47f2395a2d8d42844
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-12 19:09:55 -0400

    feature: users creation with form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    render html: 'It works'
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = 'Welcome to the Alpha Blog, you have successfully signed up.'
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end

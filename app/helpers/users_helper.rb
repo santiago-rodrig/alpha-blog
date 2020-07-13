2595263b6e40ff199394cf2fde8a7de3f44ce609
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-13 18:26:10 -0400

    feature: conditionally show user actions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -17,4 +17,17 @@ module UsersHelper
       ).html_safe
     end
   end
+
+  def render_show_user_actions(user)
+    if current_user == user
+      return %(
+        <div class="btn-group" role="group" aria-label="user actions">
+          #{link_to 'Edit', edit_user_path(@user), class: 'btn btn-dark'}
+          #{link_to 'Delete', @user, method: :delete, data: { confirm: 'Are you sure?' }, class: 'btn btn-danger'}
+        </div>
+      ).html_safe
+    end
+
+    nil
+  end
 end

a92ce7ef35cdb15bfcf769ae7d41cab0ff4918da
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-13 20:06:08 -0400

    feature: admin state for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_12_221449) do
+ActiveRecord::Schema.define(version: 2020_07_14_000457) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_07_12_221449) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "articles", "users"

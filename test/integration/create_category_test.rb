e30e8111aba70d0f9dc5ed8d67f341906db3350a
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-14 13:45:03 -0400

    feature: invalid category test

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -14,5 +14,17 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_match 'pets', response.body
   end
+
+  test 'get category form and reject invalid category' do
+    get '/categories/new'
+    assert_response :success
+
+    assert_no_difference 'Category.count' do
+      post '/categories', params: { category: { name: 'pe' } }
+    end
+
+    assert_select 'div.alert.alert-danger'
+    assert_match /errors.*category/i, response.body
+  end
 end
 

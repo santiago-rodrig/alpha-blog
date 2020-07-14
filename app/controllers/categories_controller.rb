09d0bb6718c50690b196921f1439575b0b9d679d
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-14 13:10:21 -0400

    feature: categories creation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,9 +2,26 @@ class CategoriesController < ApplicationController
   def show
   end
 
+  def create
+    @category = Category.create(category_params)
+    if @category.save
+      flash[:success] = 'Category created successfully.'
+      redirect_to @category
+    else
+      render :new
+    end
+  end
+
   def index
   end
 
   def new
+    @category = Category.new
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end

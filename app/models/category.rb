696b9fb790b4d77748d2cbf9279201b2953798e4
Santiago Rodriguez <santo1996.29@gmail.com>
2020-07-14 08:57:45 -0400

    feature: category model validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  
+  validates :name, presence: true, uniqueness: { case_sensitive: true }, length: { minimum: 3, maximum: 25 }
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -5,4 +5,25 @@ class CategoryTest < ActiveSupport::TestCase
     category = Category.new(name: 'sports')
     assert category.valid?
   end
+
+  test 'name should be present' do
+    category = Category.new(name: '')
+    assert category.invalid?
+  end
+
+  test 'name should be unique' do
+    category1 = Category.create(name: 'sports')
+    category2 = Category.new(name: 'sports')
+    assert category2.invalid?
+  end
+
+  test 'name should not be too long' do
+    category = Category.new(name: 'a' * 26)
+    assert category.invalid?
+  end
+
+  test 'name should not be too short' do
+    category = Category.new(name: 'aa')
+    assert category.invalid?
+  end
 end
